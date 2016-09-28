81c844f647823a53949ccfa41476b84518faaa80
Michał Hepner <michal.hepner@gmail.com>
2016-09-28 21:50:40 +0200

    Install grunt

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    exec: {
+      phpdoc: "./vendor/bin/phpdoc"
+    },
+    clean: {
+      phpdoc: "doc/*"
+    },
+    watch: {
+      phpdoc: {
+        files: ["src/*.*"],
+        tasks: ["clean:phpdoc", "exec:phpdoc"],
+        options: {
+          debounceDelay: 5000
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-exec");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+
+  grunt.registerTask("phpdoc", ["clean:phpdoc", "exec:phpdoc"]);
+  grunt.registerTask("default", ["phpdoc"]);
+}
